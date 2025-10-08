929c4923bee00e2e7a46dfb14592d4f49a1a899a
Galuh Dwi Pranoto <galuhdpranoto@student.untan.ac.id>
2025-10-08 12:49:53 +0700

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: "https://seowithgaluh.netlify.app",
+  integrations: [tailwind(), react(), sitemap()],
   vite: {
     resolve: {
       alias: {
