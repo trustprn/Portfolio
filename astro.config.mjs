40c686a8465f2fc4f1e7f413190165bae577e4d7
Galuh Dwi Pranoto <galuhdpranoto@student.untan.ac.id>
2025-10-08 14:30:18 +0700

    update sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,17 +12,21 @@ export default defineConfig({
     tailwind(),
     react(),
     sitemap({
-      // 🧹 Hilangkan halaman yang tidak perlu (contoh: 404)
+      //  Hilangkan halaman yang tidak perlu (contoh: 404)
       filter: (page) => !page.includes("/404"),
 
-      // ⚙️ Atur meta tambahan (SEO-friendly)
+      // Atur meta tambahan (SEO-friendly)
       serialize(item) {
         return {
           url: item.url,
-          changefreq: "weekly",
+          changefreq: "always",
           priority: item.url.endsWith("/") ? 1.0 : 0.7,
         };
       },
+    }),
+    robotsTxt({
+      policy: [{ userAgent: "*", allow: "/" }],
+      sitemap: "https://seowithgaluh.netlify.app/sitemap-index.xml",
     }),
   ],
   vite: {
